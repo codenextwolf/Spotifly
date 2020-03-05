8dc98d3cd55fc896666903588967e46d7ee0df98
leahmoreno <leah.moreno@csedge.org>
2020-03-05 17:53:57 -0500

    adds drawables and updates artist arraylist

--- app/src/main/java/com/example/android/spotifly/ArtistActivity.java
+++ app/src/main/java/com/example/android/spotifly/ArtistActivity.java
@@ -3,6 +3,9 @@ package com.example.android.spotifly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ArtistActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ArtistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
+
+        ArrayList<CategoryItem> artists = new ArrayList<>();
+        artists.add(new CategoryItem("Kid Cudi", R.drawable.cudi));
+        artists.add(new CategoryItem("Gunna", R.drawable.gunna));
+        artists.add(new CategoryItem("Rihanna", R.drawable.rihanna));
+        artists.add(new CategoryItem("YoungBoy", R.drawable.youngboy));
+
+        ListView artistListview = findViewById(R.id.artist_listview);
+
+        CategoryAdapter artistAdapter = new CategoryAdapter(this, artists);
+        artistListview.setAdapter(artistAdapter);
     }
 }
